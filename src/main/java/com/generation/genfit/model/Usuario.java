9a1d0850d16b94b2fae5a14cbc0bb1d6333b45df
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2024-12-17 11:43:27 -0300

    estruturacao do projeto

--- src/main/java/com/generation/genfit/model/Usuario.java
+++ src/main/java/com/generation/genfit/model/Usuario.java
@@ -1,5 +1,88 @@
 package com.generation.genfit.model;
 
+import javax.persistence.*;
+
+import org.hibernate.annotations.CascadeType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String usuario;
+    private String senha;
+    private String foto;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Cliente> clientes;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Oportunidade> oportunidades;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public List<Oportunidade> getOportunidades() {
+        return oportunidades;
+    }
+
+    public void setOportunidades(List<Oportunidade> oportunidades) {
+        this.oportunidades = oportunidades;
+    }
 }
