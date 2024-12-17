eb7d37fe1e220e5bc5d048255539f89078d550af
StPVieira <pedrohq.vieira168@gmail.com>
2024-12-17 13:36:05 -0300

    feat: cliente controller e cliente repository criados

---
+++ src/main/java/com/generation/gestaogen/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.generation.gestaogen.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.gestaogen.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
