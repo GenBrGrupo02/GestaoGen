0af87bd001d17f3af4a584ac537e5bb6859211b3
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2024-12-17 11:55:32 -0300

    estruturacao do projeto

--- src/main/java/com/generation/gestaogen/repository/UsuarioRepository.java
+++ src/main/java/com/generation/gestaogen/repository/UsuarioRepository.java
@@ -3,6 +3,8 @@ package com.generation.gestaogen.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.generation.gestaogen.model.Usuario;
+
 
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
